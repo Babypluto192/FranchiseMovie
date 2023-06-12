02879e376c7e56649391de9a0ddf6c7e42842abc
Ruslan <saparuslan111105@gmail.com>
2023-06-12 19:57:31 +0600

    v 6.5

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,8 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
     title: 'Damirsinba site',
     description: 'Damirsinba site',
+    httpEquiv: "Content-Security-Policy",
+    content: "upgrade-insecure-requests",
 }
 
 export default function RootLayout({
