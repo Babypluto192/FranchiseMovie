b4f5136504be560c77640b374ef9911e40300ece
Ruslan <saparuslan111105@gmail.com>
2023-06-13 22:54:23 +0600

    v 7

--- app/forum/page.tsx
+++ app/forum/page.tsx
@@ -69,7 +69,7 @@ function SignOut() {
 }
 
 function ChatRoom() {
-    const dummy = useRef<HTMLDivElement>(null);
+    const dummy = useRef<HTMLSpanElement>(null);
     const messagesRef = firestore.collection('messages')
 
     const query = messagesRef.orderBy('CreatedAt').limit(25);
