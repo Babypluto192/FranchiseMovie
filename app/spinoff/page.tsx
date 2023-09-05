c6a59fccdb65a14f3ca59a5c604dd26b7fdbba1b
Babypluto192 <saparuslan111105@gmail.com>
2023-09-05 21:01:24 +0600

    v 7.1

--- app/spinoff/page.tsx
+++ app/spinoff/page.tsx
@@ -9,8 +9,7 @@ export default async function SpinOff ()  {
 
             <h1 className={classes.h1}>Это Спин-Оффы </h1>
 
-            <Movie  title='title' rating='rating' url='https://youtu.be/dQw4w9WgXcQ?si=iQU9RDsgX2wjxg-2' preview='https://whey.kz/wp-content/uploads/2020/11/placeholder.png' film="фильма"/>
-
+            <Movie  title='title' rating='rating' url='https://youtu.be/dQw4w9WgXcQ?si=iQU9RDsgX2wjxg-2' preview="https://whey.kz/wp-content/uploads/2020/11/placeholder.png" film="фильма" id='1' />
 
 
 
--- app/trailer/page.tsx
+++ app/trailer/page.tsx
@@ -11,7 +11,7 @@ export default async function Trailer ()  {
 
             <h1 className={classes.h1}>Это трейлеры </h1>
 
-            <Movie  title='title' rating='rating' url='https://youtu.be/dQw4w9WgXcQ?si=iQU9RDsgX2wjxg-2' preview='https://whey.kz/wp-content/uploads/2020/11/placeholder.png' film="фильма"/>
+            <Movie  title='title' rating='rating' url='https://youtu.be/dQw4w9WgXcQ?si=iQU9RDsgX2wjxg-2' preview="https://whey.kz/wp-content/uploads/2020/11/placeholder.png" film="фильма" id='1' />
 
 
 
