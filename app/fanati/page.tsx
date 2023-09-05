85da09ccea95a22a30d0b49cffdddfba5eb1f85f
Babypluto192 <saparuslan111105@gmail.com>
2023-09-05 20:50:19 +0600

    v 7.1

--- app/fanati/page.tsx
+++ app/fanati/page.tsx
@@ -10,7 +10,7 @@ export default  async function Fanati ()  {
 
             <h1 className={classes.h1}>Это фанатская дейтельность </h1>
 
-            <Movie  title='title' rating='rating' url='https://youtu.be/dQw4w9WgXcQ?si=iQU9RDsgX2wjxg-2' preview='https://whey.kz/wp-content/uploads/2020/11/placeholder.png' film="фильма"/>
+            <Movie id='1' title='title' rating='rating' url='https://youtu.be/dQw4w9WgXcQ?si=iQU9RDsgX2wjxg-2' preview="https://whey.kz/wp-content/uploads/2020/11/placeholder.png" film="фильма"/>
 
 
 
